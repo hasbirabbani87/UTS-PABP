0a5fc13597afe3b1a0f7a73aba49a98ef2afc327
Hasbi Rabbani <hasbirabbani3@gmail.com>
2026-03-14 01:04:16 +0700

    update decimal on db

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,10 +4,10 @@ exports.verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.status(403).json({ message: "Token tidak disediakan!" });
 
-    const token = authHeader.split(' ')[1]; // Format: Bearer <token>
+    const token = authHeader.split(' ')[1]; 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) return res.status(401).json({ message: "Token tidak valid!" });
-        req.userId = decoded.id; // Simpan ID user ke request untuk dipakai di controller
+        req.userId = decoded.id; 
         next();
     });
 };
